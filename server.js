2ff2b550cf52d01f61b4e6fdb28aca0c86ec03c0
Amir Baghaie <amirbagh75@gmail.com>
2020-06-27 22:18:30 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 var express = require("express");
 var morgan = require("morgan");
 var mysql = require("mysql");
-const MongoClient = require("mongodb").MongoClient;
-const mongoDSN = process.env.MONGO_DSN;
 var app = express();
 var path = require("path");
-
-// MySQL DB connection
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+const MongoClient = require("mongodb").MongoClient;
+const mongoDSN = process.env.MONGO_DSN;
 
 app.use(morgan("tiny"));
 app.use(express.static("public"));
@@ -23,18 +14,27 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-connection.connect();
-connection.query("SELECT 1 + 1 AS plus", function (error, results, fields) {
-  if (error) throw error;
-  console.log("The 1+1 is: ", results[0].plus);
-});
-connection.end();
+// // MySQL DB connection
+// var connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
 
-// MongoDB connection
-MongoClient.connect(mongoDSN, function (err, client) {
-  console.log("Connected successfully to server");
-  client.close();
-});
+// connection.connect();
+// connection.query("SELECT 1 + 1 AS plus", function (error, results, fields) {
+//   if (error) throw error;
+//   console.log("The 1+1 is: ", results[0].plus);
+// });
+// connection.end();
+
+// // MongoDB connection
+// MongoClient.connect(mongoDSN, function (err, client) {
+//   console.log("Connected successfully to server");
+//   client.close();
+// });
 
 app.listen(8000, () =>
   console.log(`app listening on port 8000 on ${process.env.URL}`)
